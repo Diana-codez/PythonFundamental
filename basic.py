4ecdf8da368cfd19eb308011331518badb93feea
Diana-codez <mommy4girl@gmail.com>
2025-05-14 21:23:00 +0530

    first commit

---
+++ basic.py
@@ -0,0 +1,282 @@
+print("Hello welcome..")
+
+#indentation
+
+#Variable
+#myVariable=12
+#My_Variable="Diana"
+
+#_my_Variable=12.3
+#User_First_Name=90
+
+One,Two,Three="Hello","hai",123
+
+#text type="str"
+
+#Numeric=int,float
+
+#sequence=list,tuple,range
+
+#map=dict
+
+#boolean type= true, false
+
+#binary= byte, memoryview
+
+#none= none
+
+Surya_Information=["Surya",10,"Erode","PG,MBA"]
+
+#Data[NAME:"Suryaraj",AGE:10,CITY:"ERODE"]
+
+Number=10
+
+print(type(Number))
+
+digitValue=12.3
+
+print(type(digitValue))
+
+Username="SURYARAJ"
+
+print(type(Username))
+
+FirstName="s"
+
+print(FirstName)
+
+#Typecasting
+
+a=int("10")
+b=int(12.3)
+
+print(a,b)
+
+c=float(1)
+d=float("10.2")
+print(c,d)
+e=str(100)
+
+print(type(e))
+
+
+#variable
+
+a=10
+a="surya"
+print(a)
+
+#firstname
+#FirstName
+#First_Name
+#_FirstName
+#Name1
+
+"""
+Camel Case:
+        myVariableNameIs
+
+Pascal case:
+        MyVariableNameIs
+
+Snake case:
+        My_Variable_Name_Is
+"""
+a,b,c="one","two","three"
+
+print(a)
+print(b)
+print(c)
+
+d=e=f="Suryaraj"
+print(d)
+print(e)
+print(f)
+
+"""
+Numbers:
+        int 0,n
+        float 0.0,n.n
+Text type:
+        string"surya"
+sequence:
+        list,tuple[1,2,3,4]
+Map:Dict{name:"surya"}
+Boolean type: True,False 0,1
+None:none
+"""
+"""
+x=1
+x=8499749729830
+x=-478287847878
+
+-n 0 n
+
+y=0.1
+x=12.4736
+y=34.46378
+z=1+5j
+
+int => float,complex
+float=> int,complex
+complex=>int,float
+a=1
+b=2.3
+c=4j
+
+float(a)
+int(b)
+comple(a)
+
+"""
+
+#a=5,4
+Name='"erode"'
+Name1="'chennai'"
+print(Name)
+print(Name1)
+
+Para="python is a programming language which is used in all types of fields such as research,applications,gaming etc. Sometimes it is hacked but it provides an easier interface and very friendly to the user too"
+print(Para)
+
+print(Name[1])
+
+
+#Boolean true=1, False=0
+
+print(10<11)
+#a=10
+#a==10
+print(10<9)
+print(10==9)
+
+#Arithmetic=> +,-,/,%,*
+#Assignment=> =,+=,-=,/=,%=
+#Comparison=> ==,!=,<,>,<=,>=
+#   Logical=> and,or,not
+#Bitwise=> &,|,~
+
+#List
+Number=[1,2,3,4.5,True,"surya"]
+#findIndex
+print(Number[5])
+#NegativeIndex
+print(Number[-1])
+#Range
+print(Number[1:5])
+#EndRange
+print(Number[:4])
+#StartRange
+print(Number[2:])
+#NegativeRange
+print(Number[-3:-1])
+#findvalue
+if "surya" in Number:
+    print("yes")
+#ChangeListValue
+    Number[1]="Erode"
+    print(Number)
+#Changerange value
+Number[0:3]=["One","two","three"]
+#print(Number)
+
+#Insertdata
+#Number.insert(2,"Kia")
+#print(Number)
+#append
+Number.append("green")
+#print(Number)
+
+#Extend
+Arr=["BMW","Kia","AUDI"]
+Number.extend(Arr)
+print(Number)
+#Remove
+Number.remove("Kia")
+print(Number)
+
+#Pop(index)
+Number.pop(3)
+print(Number)
+
+#delete
+del Number[5]
+print(Number)
+
+#Clear
+Number.clear()
+print(Number)
+
+#SORT METHODS
+
+Name =["d","a","b","r","e","o"]
+Number=[34,21,18,45,90,98,32]
+
+Name.sort()
+print(Name)
+
+Number.sort()
+print(Number)
+
+Number.sort(reverse=True)
+print(Number)
+
+#Copylist
+Mylist=Name.copy()
+print(Mylist)
+#Join
+print(Name+Number)
+
+#Tubles
+N=("apple","Kia")
+print(N)
+
+#Sets
+Car={"BIKE1","Bike2","Bike3"}
+print(Car)
+
+#Dictionary
+ThisDis={
+    "NAME":"SURYA",
+    "AGE":12,
+    "CITY":"ERODE"
+    "CITY":"CHENNAI"}
+print(ThisDis)
+print(ThisDis["NAME"])
+print(len(ThisDis))
+
+#Dictionary constructor
+Disct=dict(name="cat",age=32)
+#print(Disct)
+#KEY=PROPERTY   VALUE=VALUE
+
+x=ThisDis.keys()
+print(x)
+
+#Value
+print(ThisDis.values())
+
+#items
+print(ThisDis.items()
+
+#Change Value
+ThisDis["CITY"]="NEWYORK"
+print(ThisDis)
+
+#Update
+ThisDis.update({"NAME":"VIJAY"})
+      print(ThisDis)
+
+#Add data
+ThisDis["Year"]=1996
+print(ThisDis)
+
+#Add data form update keyword:
+ThisDis.update({"Model":1999}0
+print(ThisDis)
+      
+
+
+
+
+
---
+++ basic2.py
@@ -0,0 +1,98 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/DELL/Desktop/python diana/basic.py
+Hello welcome..
+Traceback (most recent call last):
+  File "C:/Users/DELL/Desktop/python diana/basic.py", line 28, in <module>
+    Surya_Information=["Surya",10,"Erode",PG,MBA]
+NameError: name 'PG' is not defined
+
+============= RESTART: C:/Users/DELL/Desktop/python diana/basic.py =============
+Hello welcome..
+<class 'int'>
+Traceback (most recent call last):
+  File "C:/Users/DELL/Desktop/python diana/basic.py", line 38, in <module>
+    print(type(digiValue))
+NameError: name 'digiValue' is not defined. Did you mean: 'digitValue'?
+
+========= RESTART: C:/Users/DELL/Desktop/python diana/basic.py =========
+Hello welcome..
+<class 'int'>
+<class 'float'>
+
+========= RESTART: C:/Users/DELL/Desktop/python diana/basic.py =========
+Hello welcome..
+<class 'int'>
+<class 'float'>
+<class 'str'>
+s
+10 12
+1.0 10.2
+<class 'str'>
+
+========= RESTART: C:/Users/DELL/Desktop/python diana/basic.py =========
+Hello welcome..
+<class 'int'>
+<class 'float'>
+<class 'str'>
+s
+10 12
+1.0 10.2
+<class 'str'>
+surya
+one
+two
+three
+Suryaraj
+Suryaraj
+Suryaraj
+"erode"
+'chennai'
+python is a programming language which is used in all types of fields such as research,applications,gaming etc. Sometimes it is hacked but it provides an easier interface and very friendly to the user too
+e
+
+========= RESTART: C:/Users/DELL/Desktop/python diana/basic.py =========
+Hello welcome..
+<class 'int'>
+<class 'float'>
+<class 'str'>
+s
+10 12
+1.0 10.2
+<class 'str'>
+surya
+one
+two
+three
+Suryaraj
+Suryaraj
+Suryaraj
+"erode"
+'chennai'
+python is a programming language which is used in all types of fields such as research,applications,gaming etc. Sometimes it is hacked but it provides an easier interface and very friendly to the user too
+e
+True
+False
+False
+surya
+surya
+[2, 3, 4.5, True]
+[1, 2, 3, 4.5]
+[3, 4.5, True, 'surya']
+[4.5, True]
+yes
+[1, 'Erode', 3, 4.5, True, 'surya']
+['One', 'two', 'three', 4.5, True, 'surya', 'green', 'BMW', 'Kia', 'AUDI']
+['One', 'two', 'three', 4.5, True, 'surya', 'green', 'BMW', 'AUDI']
+['One', 'two', 'three', True, 'surya', 'green', 'BMW', 'AUDI']
+['One', 'two', 'three', True, 'surya', 'BMW', 'AUDI']
+[]
+['a', 'b', 'd', 'e', 'o', 'r']
+[18, 21, 32, 34, 45, 90, 98]
+[98, 90, 45, 34, 32, 21, 18]
+['a', 'b', 'd', 'e', 'o', 'r']
+['a', 'b', 'd', 'e', 'o', 'r', 98, 90, 45, 34, 32, 21, 18]
+('apple', 'Kia')
+{'Bike3', 'BIKE1', 'Bike2'}
+SURYA
